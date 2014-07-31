db9860fd276cb3057d96b69a690e110903ae1fd8
jeffwesson <jeff@jeffwesson.com>
2014-07-31 04:00:32 -0700

    added data model

--- app/scripts/services/data-model.js
+++ app/scripts/services/data-model.js
@@ -8,13 +8,14 @@
  * Factory in the jeffwessonApp.
  */
 angular.module('jeffwessonApp')
-  .factory('dataModel', function ($http) {
-    var dataModel = {};
-    dataModel.getData = function () {
-      $http.get('./scripts/models/data-model.json')
-        .success(function (response) {
-          dataModel.content = response ? response : [];
-        });
-    };
-    return dataModel;
-  });
+  .factory('dataModel', [ '$http',
+    function ($http) {
+      var dataModel = {};
+      dataModel.getData = function () {
+        $http.get('./scripts/models/data-model.json')
+          .success(function (response) {
+            dataModel.content = response ? response : [];
+          });
+      };
+      return dataModel;
+    }]);
