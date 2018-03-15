411dffd7169931c0d70fee3b0f44bb030b1edd83
jeffwesson <jeff@jeffwesson.com>
2018-03-14 18:13:21 -0700

    add favicon

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ export const portfolio = {
 			"description": "Worked with a team to develop and maintain a web experience and platform with which both Audi customers as well as Audi Brand Specialists could interact.",
 			"image": "/images/audiusa-devices.jpg",
 			"inDevelopment": false,
-			"address": "http://audiusa.com",
+			"address": "https://audiusa.com",
 			"toolsUsed": [
 				"Backbone.js",
 				"jQuery",
@@ -25,7 +25,6 @@ export const portfolio = {
 			"description": "Worked with a team to develop enhancements for Levi's Vintage Clothing.",
 			"image": "/images/lvc-grid.png",
 			"inDevelopment": false,
-			"address": "http://levisvintageclothing.levi.com",
 			"toolsUsed": [
 				"AngularJS",
 				"HTML5",
@@ -40,7 +39,7 @@ export const portfolio = {
 			"description": "Working with a small team of 2-3 developers on a sprint planning solution for agile and scrum development teams. Planama uses bleeding edge technologies including realtime database syncing, through Firebase, to achieve a much more lightweight and easy-to-iterate product.",
 			"image": "/images/planama.png",
 			"inDevelopment": true,
-			"address": "http://planama.firebaseapp.com",
+			"address": "https://planama.firebaseapp.com",
 			"toolsUsed": [
 				"AngularJS",
 				"Firebase",
@@ -117,7 +116,6 @@ export const portfolio = {
 			"description": "I designed a fairly simple, mobile-first, responsive theme to accommodate a blog. I opted for super simplicity and only incorporated bare necessities such as recent blog posts, search, and archives based on categories and tags. I have since, however, relocated to Medium and Twitter for any writing needs.",
 			"image": "/images/jeffwesson.png",
 			"inDevelopment": false,
-			"address": "http://jeffwesson.com",
 			"toolsUsed": [
 				"PHP",
 				"HTML5",
@@ -135,7 +133,6 @@ export const portfolio = {
 			"description": "Worked closely with a designer to achieve a pixel-perfect representation of their vision while incorporating common UI thinking and best practices. Using WordPress-as-a-CMS, I built a menu system for updating items that doesn't just produce a pdf, which can be severely lacking concerning accessibility.",
 			"image": "/images/gypsycafe.png",
 			"inDevelopment": false,
-			"address": "http://gypsy-cafe.com",
 			"toolsUsed": [
 				"PHP",
 				"HTML5",
@@ -148,7 +145,6 @@ export const portfolio = {
 			"description": "Version 1.0 of a collaborative effort to build more appealing, informative, useful, and functional websites for local businesses in Sonoma County, California. This project uses semantic HTML5 and cleverly laid-out CSS3 to accomplish a clean and minimalistic approach to web development.",
 			"image": "/images/wdesigns.png",
 			"inDevelopment": false,
-			"address": "http://wdesigns.co",
 			"toolsUsed": [
 				"HTML5",
 				"CSS3"
